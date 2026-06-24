0ea2b1ff0359bbc310622cc55fb5a0a76876d5fa
Karthick Raja <mkkarthickraja07@gmail.com>
2026-06-24 11:29:50 +0530

    Modified Files in all Phase Documentations

--- Python/export_dashboard_data.py
+++ Python/export_dashboard_data.py
@@ -53,6 +53,41 @@ queries = {
 
     SELECT 'Hires', COUNT(*)
     FROM Hires;
+    """,
+
+    "Recruiter_Performance":
+    """
+    SELECT
+        r.Recruiter_Name,
+        COUNT(j.Job_ID) AS Jobs_Managed
+    FROM Recruiters r
+    LEFT JOIN Jobs j
+        ON r.Recruiter_ID = j.Recruiter_ID
+    GROUP BY r.Recruiter_Name
+    ORDER BY Jobs_Managed DESC;
+    """,
+
+    "Executive_Hiring_Summary":
+    """
+    SELECT
+        c.Company_Name,
+        COUNT(DISTINCT a.Application_ID) AS Applications,
+        COUNT(DISTINCT i.Interview_ID) AS Interviews,
+        COUNT(DISTINCT o.Offer_ID) AS Offers,
+        COUNT(DISTINCT h.Hire_ID) AS Hires
+    FROM Companies c
+    LEFT JOIN Jobs j
+        ON c.Company_ID = j.Company_ID
+    LEFT JOIN Applications a
+        ON j.Job_ID = a.Job_ID
+    LEFT JOIN Interviews i
+        ON a.Application_ID = i.Application_ID
+    LEFT JOIN Offers o
+        ON a.Application_ID = o.Application_ID
+    LEFT JOIN Hires h
+        ON o.Offer_ID = h.Offer_ID
+    GROUP BY c.Company_Name
+    ORDER BY Hires DESC;
     """
 }
 
